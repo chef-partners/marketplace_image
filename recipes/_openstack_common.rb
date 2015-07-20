65ba6d43fcacc1cb2b5f2900c60acfb80db670ed
JJ Asghar <jj@getchef.com>
2015-07-20 15:07:36 -0500

    inital openstack commit

---
+++ recipes/_openstack_common.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: marketplace_image
+# Recipe:: _openstack_common
+#
+# Copyright (C) 2015 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+file '/etc/chef/ohai/hints/openstack.json' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create_if_missing
+end
+
+user 'openstack-user' do
+  home '/home/openstack-user'
+  shell '/bin/bash'
+  action [:create, :lock]
+end
+
+package 'cloud-init' do
+  action :install
+end
+
+template '/etc/cloud/cloud.cfg' do
+  source 'openstack-cloud-init.erb'
+  cookbook 'marketplace_image'
+  action :create
+end
+
+include_recipe 'marketplace_image::_openstack_security'
