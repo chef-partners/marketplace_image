6a3b2b6e8382df01096842c4efb8dabed4054534
Adam Leff <adamleff@chef.io>
2016-01-22 16:48:54 -0500

    moving runlist/attribute logice from marketplace_gce back here

--- recipes/gce_publisher.rb
+++ recipes/gce_publisher.rb
@@ -18,7 +18,7 @@
 #
 
 # ensure required info is in Chef::Config
-required_keys = %w(credentials_file chef_server validator_name validator_file ssh_username)
+required_keys = %w(credentials_file chef_server validation_client_name validation_key ssh_username)
 missing_keys = required_keys.each_with_object([]) do |item, memo|
   memo << item unless node['gce'].key?(item)
 end
@@ -39,37 +39,31 @@ gce_images = node['marketplace_image']['gce'][marketplace][product]['products'].
   memo << item
 end
 
-# Create the images
 gce_images.each do |image|
+  node_attributes = {
+    'marketplace_image' => {
+      'role' => role,
+      'platform' => 'gce',
+      'license_count'     => image['node_count'],
+      'license_type'      => license_type,
+      'support_email'     => node['marketplace_image']['gce'][role]['support_email'],
+      'doc_url'           => node['marketplace_image']['gce'][role]['doc_url'],
+      'publishing_bucket' => 'chef-partners-test-images'
+    }
+  }
+
   marketplace_image_gce image['name'] do
-    snapshot             true
-    destroy_after        true
-    gce_credentials_file node['gce']['credentials_file']
-    project              'chef-partners-test'
-    chef_server_url      node['gce']['chef_server']
-    validator_name       node['gce']['validator_name']
-    validator_key_file   node['gce']['validator_file']
-    instance_type        'n1-standard-4'
-    source_image_id      image_id
-    ssh_username         node['gce']['ssh_username']
-    ssh_key_file         node['gce']['ssh_key_file'] if node['gce']['ssh_key_file']
-    marketplace_role     role
-    license_type         license_type
-    license_count        image['node_count']
-    support_email        node['marketplace_image']['gce'][role]['support_email']
-    doc_url              node['marketplace_image']['gce'][role]['doc_url']
+    destroy_after          true
+    gce_credentials_file   node['gce']['credentials_file']
+    project                'chef-partners-test'
+    chef_server_url        node['gce']['chef_server']
+    validation_client_name node['gce']['validation_client_name']
+    validation_key         node['gce']['validation_key']
+    instance_type          'n1-standard-4'
+    source_image_id        image_id
+    ssh_username           node['gce']['ssh_username']
+    ssh_key_file           node['gce']['ssh_key_file'] if node['gce']['ssh_key_file']
+    run_list               [ 'marketplace_gce::bootstrap', 'marketplace_image::_publisher', 'marketplace_gce::create_snapshot' ]
+    node_attributes        node_attributes
   end
 end
-
-# Build manifest
-#manifest = proc do
-#  node.run_state['marketplace_gce_images'].each_with_object([]) do |image, memo|
-#    memo << item
-#  end
-#end
-
-# Write manifest
-#file File.expand_path(File.join('~', "gce_#{role}_#{marketplace}_images.json")) do
-#  content lazy { Chef::JSONCompat.to_json_pretty(manifest.call) }
-#  action :create
-#end
